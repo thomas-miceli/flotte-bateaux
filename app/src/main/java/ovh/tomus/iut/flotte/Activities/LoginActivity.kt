76c2a72f669eb8d67a906c99203dc892cb68b0d9
quentin-pla <quentin-pla@hotmail.fr>
2019-03-06 15:18:36 +0100

    Ajout création utilisateur avec Firebase

--- app/src/main/java/ovh/tomus/iut/flotte/Activities/LoginActivity.kt
+++ app/src/main/java/ovh/tomus/iut/flotte/Activities/LoginActivity.kt
@@ -5,26 +5,33 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 import ovh.tomus.iut.flotte.R
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
+    var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
-        // Initialize Firebase Auth
-        auth = FirebaseAuth.getInstance()
     }
 
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
+    fun submit(view: View) {
+        val email = inputMail.text.toString()
+        val mdp = inputMdp.text.toString()
+        firebaseAuth.createUserWithEmailAndPassword(email,mdp).addOnCompleteListener{task ->
+            if(task.isSuccessful){
+                Toast.makeText(applicationContext,"Utilisateur créé avec succès",Toast.LENGTH_LONG).show()
+            }
+            else {
+                Toast.makeText(applicationContext,"Erreur de création de l'utilisateur",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 }
