5f35dcb7c533aa65fd70c73530432617aaa63305
Benoit Joubert <benoit.joubert@etu.univ-amu.fr>
2019-03-06 17:01:46 +0100

    Ajout des fichiers pour Google Maps

--- app/src/main/java/ovh/tomus/iut/flotte/Models/Containership.kt
+++ app/src/main/java/ovh/tomus/iut/flotte/Models/Containership.kt
@@ -2,7 +2,5 @@ package ovh.tomus.iut.flotte.Models
 
 class Containership (var idBateau: Int,var nomBateau: String,var captainName: String, var latitude: Float, var longitude: Float, var port: Port, var type: ContainershipType, var containers: Collection<Container> ){
 
-
-
 }
 
