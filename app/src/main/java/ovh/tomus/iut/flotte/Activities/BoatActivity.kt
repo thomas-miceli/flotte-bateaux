aa407654570b834cd8d065c121ddcc48ede9b5a2
Benoit Joubert <benoit.joubert@etu.univ-amu.fr>
2019-03-22 12:29:57 +0100

    Rangement fichier éditeur bateau

--- app/src/main/java/ovh/tomus/iut/flotte/Activities/BoatActivity.kt
+++ app/src/main/java/ovh/tomus/iut/flotte/Activities/BoatActivity.kt
@@ -2,14 +2,12 @@ package ovh.tomus.iut.flotte.Activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_boat.*
-import kotlinx.android.synthetic.main.activity_first.*
 import ovh.tomus.iut.flotte.Models.*
 import ovh.tomus.iut.flotte.R
 
 class BoatActivity : AppCompatActivity() {
-    val port =  Port(1, "Xd", 50.0f, 80.0f)
+    val port =  Harbor(1, "Xd", 50.0f, 80.0f)
     val containershipType = ContainershipType(1, 50, 410, 60)
     val collection : Collection<Container> = listOf()
     val containership = Containership(1, "xD", "xD2", 50.0f, 40.0f, port, containershipType, collection)
