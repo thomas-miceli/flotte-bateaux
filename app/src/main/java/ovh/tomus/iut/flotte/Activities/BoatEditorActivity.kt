0e422bbf12a51817c0bb1d08e8925081f53839fc
JOUBERT Benoit <j17024873@a-maq-iutinfo.salsa.univ-amu.fr>
2019-03-22 10:13:39 +0100

    débuggage de bateau éditeur

--- app/src/main/java/ovh/tomus/iut/flotte/Activities/BoatEditorActivity.kt
+++ app/src/main/java/ovh/tomus/iut/flotte/Activities/BoatEditorActivity.kt
@@ -3,29 +3,21 @@ package ovh.tomus.iut.flotte.Activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.content_boat_editor.*
 import ovh.tomus.iut.flotte.R
 
-import kotlinx.android.synthetic.main.activity_boateditor.*
-import kotlinx.android.synthetic.main.content_boateditor.*
-
-class BoateditorActivity : AppCompatActivity() {
+class BoatEditorActivity : AppCompatActivity() {
 
     val db = FirebaseFirestore.getInstance()
-    //val boatName = findViewById<View>(R.id.editText_boatName)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_boateditor)
-        setSupportActionBar(toolbar)
-        }
-
+        setContentView(R.layout.activity_boat_editor)
+    }
 
     fun updateData (view: View){
-        val boatName = editText_boatName.text.toString()
+        val boatName = edit_boatname.text.toString()
         val containerShipRef = db.collection("containerShips").document("JEaTarJj3Yg9aaRJTIct")
         containerShipRef.update("boatName", boatName)
     }
