f798780889d84047056f4af82be43387f3d56e5a
thomas <tomus.mic@gmail.com>
2019-03-28 03:26:58 +0100

    Push des données faite de façon propre

--- app/src/main/java/ovh/tomus/iut/flotte/Activities/AddBoatActivity.kt
+++ app/src/main/java/ovh/tomus/iut/flotte/Activities/AddBoatActivity.kt
@@ -9,6 +9,7 @@ import kotlinx.android.synthetic.main.activity_addboat.*
 import ovh.tomus.iut.flotte.R
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.google.firebase.firestore.DocumentReference
 
 
 class AddBoatActivity : AppCompatActivity() {
@@ -30,7 +31,6 @@ class AddBoatActivity : AppCompatActivity() {
             if (task.isSuccessful) {
                 for (harbour in task.result!!) {
                     harbourList[harbour.data["name"].toString()] = harbour.id
-                    Toast.makeText(applicationContext,harbour.data["name"].toString(), Toast.LENGTH_SHORT).show()
                 }
                 val adapter = ArrayAdapter(
                     this, android.R.layout.simple_spinner_item, harbourList.keys.toTypedArray()
@@ -42,22 +42,34 @@ class AddBoatActivity : AppCompatActivity() {
     }
 
     fun addBoat(view: View) {
-        val boatName : String = boatname.text.toString()
-        val captainName : String = captainname.text.toString()
-        val localization = GeoPoint(editText7.text.toString().toDouble(), editText8.text.toString().toDouble())
-        val harbour : String = harbourList.get(harbourspinner.selectedItem).toString()
-        val data = HashMap<String, Any>()
 
-        data["boatName"] = boatName
-        data["captainName"] = captainName
-        data["localization"] = localization
-        data["port"] = harbour
-        boats.add(data)
+        val boatName: String = boatname.text.toString()
+        val captainName: String = captainname.text.toString()
+        val latitude : String = latitudeInput.text.toString()
+        val longitude : String = longitudeInput.text.toString()
+        val harbour: DocumentReference = harbours.document(harbourList.get(harbourspinner.selectedItem).toString())
 
-        Toast.makeText(this, "Bateau ajouté avec succès", Toast.LENGTH_SHORT).show()
-        finish()
+        if (boatName.isNotBlank() && captainName.isNotBlank() && latitudeInput.text.isNotBlank() && longitudeInput.text.isNotBlank()) {
 
+            if (latitude.toDouble() >= -90 && latitude.toDouble() <= 90 && longitude.toDouble() >= -180 && longitude.toDouble() <= 180) {
+                val localization = GeoPoint(latitude.toDouble(), longitude.toDouble())
 
+                val data = HashMap<String, Any>()
+
+                data["boatName"] = boatName
+                data["captainName"] = captainName
+                data["localization"] = localization
+                data["port"] = harbour
+                boats.add(data)
+
+                Toast.makeText(this, "Bateau ajouté avec succès", Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                Toast.makeText(this, "Coordonnées impossibles", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, "Merci de remplir tous les champs", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
