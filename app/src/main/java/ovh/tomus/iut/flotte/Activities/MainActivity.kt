4cda557131d059943c4107ab1d797d470336fd0b
benoit-joubert <benoit.joubert@etu.univ-amu.fr>
2019-04-06 16:24:03 +0200

    Ajout Crashlytics + bouton crash test

--- app/src/main/java/ovh/tomus/iut/flotte/Activities/MainActivity.kt
+++ app/src/main/java/ovh/tomus/iut/flotte/Activities/MainActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.crashlytics.android.Crashlytics
+import kotlinx.android.synthetic.main.activity_main.*
 import ovh.tomus.iut.flotte.R
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +15,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val crashButton = Button(this)
+        crashButton.text = "Crash!"
+        crashButton.setOnClickListener {
+            Crashlytics.getInstance().crash() // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
     }
 
     fun click(view: View) {
